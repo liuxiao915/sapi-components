a3a2502ec7b390b67369eb932b58ab8bae17ce94
liuxiao <1054105865@qq.com>
2022-12-30 11:15:33 +0800

    update: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <sapi-swiper />
+</template>
+
+<script>
+import SapiSwiper from './components/sapi-swiper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SapiSwiper
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
